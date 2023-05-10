58823075b43745a8bfc49185f104e7bd903bcb8e
Jared Finn <jaredhfinn@gmail.com>
2023-05-09 22:06:33 -0400

    home page and account page start

--- Components/accountScreen.tsx
+++ Components/accountScreen.tsx
@@ -1,13 +1,85 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
 
-
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1 }}>
-        <Text>Account Screen</Text>
+const AccountScreen = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.title}>My Account</Text>
       </View>
-    );
-  }
-}
+      <View style={styles.content}>
+        <View style={styles.profile}>
+          <Image
+            style={styles.profilePic}
+            source={{ uri: '/' }}
+          />
+          <Text style={styles.profileName}>John Smith</Text>
+          <Text style={styles.profileHeader}>Software Engineer</Text>
+          <Text style={styles.profileBio}>I love building apps that make people's lives easier.</Text>
+        </View>
+        <Text style={styles.sectionTitle}>About Me</Text>
+        <Text style={styles.sectionText}>I'm a software engineer with over 5 years of experience in developing mobile and web applications. My passion is to build products that solve real-world problems and improve people's lives. In my free time, I enjoy hiking, reading, and trying out new restaurants.</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFA500',
+  },
+  header: {
+    height: 80,
+    backgroundColor: '#FF8C00',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#FFF',
+  },
+  content: {
+    flex: 1,
+    padding: 20,
+  },
+  profile: {
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  profilePic: {
+    width: 120,
+    height: 120,
+    borderRadius: 60,
+    marginBottom: 10,
+  },
+  profileName: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  profileHeader: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#666',
+    marginBottom: 5,
+  },
+  profileBio: {
+    fontSize: 16,
+    textAlign: 'center',
+    marginHorizontal: 50,
+    marginBottom: 20,
+  },
+  sectionTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  sectionText: {
+    fontSize: 16,
+    marginBottom: 20,
+  },
+});
+
+export default AccountScreen;
--- Components/homeScreen.tsx
+++ Components/homeScreen.tsx
@@ -1,13 +1,38 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, Image } from 'react-native';
 
-
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1 }}>
-        <Text>Home Screen</Text>
+const HomeFeedPage = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        
       </View>
-    );
-  }
-}
+      <ScrollView style={styles.content}>
+        
+      </ScrollView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFF',
+  },
+  header: {
+    height: 80,
+    backgroundColor: '#FF5700',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logo: {
+    width: 60,
+    height: 60,
+  },
+  content: {
+    flex: 1,
+    padding: 20,
+  },
+});
+
+export default HomeFeedPage;
